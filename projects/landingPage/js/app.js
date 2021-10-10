2eca983dd6b2f8e8bf02af38d5b30ebd9ebd2d2e
Kareem00101 <89580954+Kareem00101@users.noreply.github.com>
2021-10-10 18:50:33 +0200

    filling the navbar with items
    created two functions, createListItem() which takes 3 attributes but returns nothing, and a function to fill the navbar by appending created listItems to it.

--- projects/landingPage/js/app.js
+++ projects/landingPage/js/app.js
@@ -14,18 +14,41 @@
 */
 
 /**
- * Define Global Variables
- * 
+ * Global Variables Definition
 */
-document.addEventListener('click', function(){
-    console.log('hello');
-});
+
+// Add all the sections in the index.html to indexSections
+// querySelectorAll(), to return a list of the sections
+const indexSections = document.querySelectorAll('section');
+
+// Getting our navbar list, from index.html
+const indexNavbar = document.querySelector('#navbar__list');
+
+
 
 /**
- * End Global Variables
  * Start Helper Functions
  * 
 */
+// Function for creating list item.
+function createListItem(listItem, id, name){
+    // Template literals were used, so that we can use the javascript variables.
+    // The code below, links the item, to the section id and provides it with the section name.
+    listItem.innerHTML = `<a class = "section__link" href = '#${id}'>${name}</a>`;
+}
+// The index navbar__list is empty we gotta fill it!
+function fillIndexNavbar(){
+    // A nice not for you ;)
+    // for of loop is 24% faster than for each according to stackoverflow.
+    for(e of indexSections){
+        // Creating a list item, for the navbar__list
+        let listItem = document.createElement('li');
+        // calling helping function createListItem, which takes three attributes
+        createListItem(listItem, e.getAttribute('id'), e.getAttribute('data-nav'));
+        // appending the listItem after they been fully created to the navbar
+        indexNavbar.appendChild(listItem);
+    }
+}
 
 
 
