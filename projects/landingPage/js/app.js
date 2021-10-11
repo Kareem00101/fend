c909942217c5be3fecb57cd8e69946510058fe2c
Kareem00101 <89580954+Kareem00101@users.noreply.github.com>
2021-10-11 08:38:01 +0200

    activateSection functionality implemented
    enhanced code readability, added activation functionality, removed unnecessary variable.

--- projects/landingPage/js/app.js
+++ projects/landingPage/js/app.js
@@ -24,11 +24,6 @@ const indexSections = document.querySelectorAll('section');
 // Getting our navbar list, from index.html
 const indexNavbar = document.querySelector('#navbar__list');
 
-// Variable to see if an element is in viewport or not
-let isVisible = false;
-
-
-
 /**
  * Start Helper Functions
  * 
@@ -39,19 +34,7 @@ function createListItem(listItem, id, name){
     // The code below, links the item, to the section id and provides it with the section name.
     listItem.innerHTML = `<a class = "nav__link" href = '#${id}'>${name}</a>`;
 }
-// The index navbar__list is empty we gotta fill it!
-function fillIndexNavbar(){
-    // A nice not for you ;)
-    // for of loop is 24% faster than for each according to stackoverflow.
-    for(ele of indexSections){
-        // Creating a list item, for the navbar__list
-        let listItem = document.createElement('li');
-        // calling helping function createListItem, which takes three attributes
-        createListItem(listItem, ele.getAttribute('id'), ele.getAttribute('data-nav'));
-        // appending the listItem after they been fully created to the navbar
-        indexNavbar.appendChild(listItem);
-    }
-}
+
 
 // function to test visibility
 function isVisible(ele){
@@ -69,11 +52,34 @@ function isVisible(ele){
  * Begin Main Functions
  * 
 */
-
 // build the nav
 
+// The index navbar__list is empty we gotta fill it!
+function fillIndexNavbar(){
+    // A nice not for you ;)
+    // for of loop is 24% faster than for each according to stackoverflow.
+    for(ele of indexSections){
+        // Creating a list item, for the navbar__list
+        let listItem = document.createElement('li');
+        // calling helping function createListItem, which takes three attributes
+        createListItem(listItem, ele.getAttribute('id'), ele.getAttribute('data-nav'));
+        // appending the listItem after they been fully created to the navbar
+        indexNavbar.appendChild(listItem);
+    }
+}
 
 // Add class 'active' to section when near top of viewport
+// Looking into the CSS, you can find an active section class
+// Using this class and simple logic we can achieve the required purpose
+function activateSection(sections){
+    // First we need to loop on our sections
+    for (ele of sections){
+        // Now we gotta check which one is visible?
+        if((isVisible(ele)) && (ele.classList.contains('your-active-class'))) continue;
+        if(isVisible(ele)) ele.classList.add('your-active-class'); else ele.classList.remove('your-active-class');
+        // Hmm, i suppose those if conditions can be handled better #note for future improvement.
+    }
+}
 
 
 // Scroll to anchor ID using scrollTO event
